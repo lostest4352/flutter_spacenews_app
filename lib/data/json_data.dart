5622482eff41e265ab64062456711dcf1ddcc3c5
user <username@email.com>
2023-08-23 19:56:54 +0545

    added new created files missed to add earlier

---
+++ lib/data/json_data.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_api_1/models/posts_model.dart';
+import 'package:http/http.dart' as http;
+
+// Future<Posts> getDataFromJson() async {
+//   final url = Uri.parse("https://jsonplaceholder.typicode.com/users/1/posts");
+//   final response = await http.get(url);
+//   if (response.statusCode == 200) {
+//     List jsonResponse = jsonDecode(response.body);
+//     return Posts.fromJson(jsonResponse[1]);
+//   } else {
+//     throw Exception("An error occured");
+//   }
+// }
+
+Future<List<Posts>> getListFromJson() async {
+  final url = Uri.parse("https://jsonplaceholder.typicode.com/users/1/posts");
+  final response = await http.get(url);
+  List<Posts> postsList = [];
+  if (response.statusCode == 200) {
+    
+    List jsonResponses = jsonDecode(response.body);
+    for (final jsonResponse in jsonResponses) {
+      final singleResponse = Posts.fromJson(jsonResponse);
+      postsList.add(singleResponse);
+    }
+    return postsList;
+    // List jsonResponse = jsonDecode(response.body);
+    // List<Posts> postsList = jsonResponse.map((data) => Posts.fromJson(data)).toList();
+    // return postsList;
+  } else {
+    throw Exception("List error occured");
+  }
+}
---
+++ lib/models/posts_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+class Posts {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+  Posts({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Posts.fromMap(Map<String, dynamic> map) {
+    return Posts(
+      userId: map['userId']?.toInt() ?? 0,
+      id: map['id']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+      body: map['body'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  // factory Posts.fromJson(String source) => Posts.fromMap(json.decode(source));
+  factory Posts.fromJson(Map<String, dynamic> map) {
+    return Posts(
+      userId: map['userId'],
+      id: map['id'],
+      title: map['title'],
+      body: map['body'],
+    );
+  }
+}
