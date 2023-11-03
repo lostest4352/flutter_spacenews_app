06970293cee07436f4affba5c3bafce8c43a89d4
user <username@email.com>
2023-11-04 02:51:01 +0545

    removed unused animations

--- lib/pages/scroll_version.dart
+++ lib/pages/scroll_version.dart
@@ -61,8 +61,7 @@ class _ScrollHomeState extends State<ScrollHome>
 
     setState(() {
       newsList?.addAll(moreNews);
-      boolNotifier.tileClicked =
-          List<bool>.filled(newsList?.length ?? 0, false);
+      boolNotifier.tileClicked = List.filled(newsList?.length ?? 0, false);
     });
   }
 
@@ -76,7 +75,7 @@ class _ScrollHomeState extends State<ScrollHome>
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("News App"),
+        title: const Text("Space News App"),
         actions: [
           IconButton(
             tooltip: 'Go back to Page 1',
@@ -102,7 +101,6 @@ class _ScrollHomeState extends State<ScrollHome>
               selectedItems.add(boolItem);
             }
           }
-
           return Column(
             children: [
               Card(
@@ -146,8 +144,6 @@ class _ScrollHomeState extends State<ScrollHome>
                         child: Text("No Internet Connection"),
                       );
                     }
-                    //
-                    // newsList = (newsList == null) ? snapshot.data : newsList;
 
                     if (!snapshot.hasData) {
                       return const Center(
@@ -159,17 +155,13 @@ class _ScrollHomeState extends State<ScrollHome>
                       if (snapshot.data != null) {
                         newsList?.addAll(snapshot.data!);
                         boolNotifier.tileClicked =
-                            List<bool>.filled(newsList?.length ?? 0, false);
+                            List.filled(newsList?.length ?? 0, false);
                       }
                     }
 
                     int itemlength = (newsList?.length ?? 0);
                     return RefreshIndicator(
                       onRefresh: () {
-                        // newsFromApi = getListFromNews(offset);
-                        // setState(() {});
-                        //
-                        // changePage(offset);
                         setState(() {
                           newsList = [];
                           offset = 0;
@@ -177,103 +169,97 @@ class _ScrollHomeState extends State<ScrollHome>
                         });
                         return newsFromApi;
                       },
-                      child: AnimatedOpacity(
-                        opacity: isVisible ? 1.0 : 0,
-                        duration: const Duration(milliseconds: 700),
-                        child: Column(
-                          children: [
-                            Flexible(
-                              child: ListView.separated(
-                                controller: scrollController,
-                                separatorBuilder: (context, index) {
-                                  return const Divider();
-                                },
-                                itemCount: itemlength + 1,
-                                itemBuilder: (context, index) {
-                                  if (index < itemlength) {
-                                    if (boolNotifier.tileClicked.isEmpty) {
-                                      List<bool> listTileBool = [];
-                                      for (int i = 0; i < itemlength; i++) {
-                                        listTileBool.add(false);
-                                        boolNotifier.tileClicked = listTileBool;
-                                      }
+                      child: Column(
+                        children: [
+                          Flexible(
+                            child: ListView.separated(
+                              controller: scrollController,
+                              separatorBuilder: (context, index) {
+                                return const Divider();
+                              },
+                              itemCount: itemlength + 1,
+                              itemBuilder: (context, index) {
+                                if (index < itemlength) {
+                                  if (boolNotifier.tileClicked.isEmpty) {
+                                    List<bool> listTileBool = [];
+                                    for (int i = 0; i < itemlength; i++) {
+                                      listTileBool.add(false);
+                                      boolNotifier.tileClicked = listTileBool;
                                     }
-
-                                    return ListTile(
-                                      selected: true,
-                                      selectedTileColor: (boolNotifier
-                                                      .tileClicked.length >=
-                                                  index &&
-                                              boolNotifier.tileClicked[index])
-                                          ? selectedColor
-                                          : null,
-                                      onLongPress: () {
-                                        boolNotifier.changeListTileState(index);
-                                      },
-                                      onTap: () {
-                                        if (boolNotifier.tileClicked
-                                            .contains(true)) {
-                                          boolNotifier
-                                              .changeListTileState(index);
-
-                                          List itemsContainingTrue = [];
-                                          for (final boolItem
-                                              in boolNotifier.tileClicked) {
-                                            if (boolItem == true) {
-                                              itemsContainingTrue.add(boolItem);
-                                            }
-                                          }
-                                          debugPrint(
-                                              "selected items length: ${itemsContainingTrue.length.toString()}");
-                                        } else {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) {
-                                                return NewsPage(
-                                                  news:
-                                                      newsList?[index] as News,
-                                                );
-                                              },
-                                            ),
-                                          );
-                                        }
-                                      },
-                                      title: Text(
-                                        newsList?[index].title ?? "no data",
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      subtitle: Text(
-                                        newsList?[index].summary ?? "no data",
-                                        style:
-                                            const TextStyle(color: Colors.blue),
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      leading: ClipRRect(
-                                        child: CachedNetworkImage(
-                                          imageUrl:
-                                              newsList?[index].image_url ?? "",
-                                          height: 100,
-                                          width: 80,
-                                          fit: BoxFit.cover,
-                                          errorWidget: (context, url, error) {
-                                            debugPrint(error.toString());
-                                            return Container();
-                                          },
-                                        ),
-                                      ),
-                                    );
-                                  } else {
-                                    return const Center(
-                                      child: CircularProgressIndicator(),
-                                    );
                                   }
-                                },
-                              ),
+
+                                  return ListTile(
+                                    selected: true,
+                                    selectedTileColor:
+                                        (boolNotifier.tileClicked.length >=
+                                                    index &&
+                                                boolNotifier.tileClicked[index])
+                                            ? selectedColor
+                                            : null,
+                                    onLongPress: () {
+                                      boolNotifier.changeListTileState(index);
+                                    },
+                                    onTap: () {
+                                      if (boolNotifier.tileClicked
+                                          .contains(true)) {
+                                        boolNotifier.changeListTileState(index);
+
+                                        List itemsContainingTrue = [];
+                                        for (final boolItem
+                                            in boolNotifier.tileClicked) {
+                                          if (boolItem == true) {
+                                            itemsContainingTrue.add(boolItem);
+                                          }
+                                        }
+                                        debugPrint(
+                                            "selected items length: ${itemsContainingTrue.length.toString()}");
+                                      } else {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) {
+                                              return NewsPage(
+                                                news: newsList?[index] as News,
+                                              );
+                                            },
+                                          ),
+                                        );
+                                      }
+                                    },
+                                    title: Text(
+                                      newsList?[index].title ?? "no data",
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    subtitle: Text(
+                                      newsList?[index].summary ?? "no data",
+                                      style:
+                                          const TextStyle(color: Colors.blue),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    leading: ClipRRect(
+                                      child: CachedNetworkImage(
+                                        imageUrl:
+                                            newsList?[index].image_url ?? "",
+                                        height: 100,
+                                        width: 80,
+                                        fit: BoxFit.cover,
+                                        errorWidget: (context, url, error) {
+                                          debugPrint(error.toString());
+                                          return Container();
+                                        },
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
+                              },
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     );
                   },
